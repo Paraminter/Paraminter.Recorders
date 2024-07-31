d162eca6606fada4b5d618412e3e9eb6f0fdcdef
ErikWe <erikwestberg7@gmail.com>
2024-07-31 17:11:27 +0200

    Include data-record in record-commands

--- src/Commands/IRecordArgumentDataCommand.cs
+++ src/Commands/IRecordArgumentDataCommand.cs
@@ -3,11 +3,15 @@
 using Paraminter.Commands;
 
 /// <summary>Represents a command to record data about the argument of a parameter.</summary>
+/// <typeparam name="TRecord">The type representing the record to which data about the argument is recorded.</typeparam>
 /// <typeparam name="TParameter">The type representing the parameter.</typeparam>
 /// <typeparam name="TArgumentData">The type representing data about the argument.</typeparam>
-public interface IRecordArgumentDataCommand<out TParameter, out TArgumentData>
+public interface IRecordArgumentDataCommand<out TRecord, out TParameter, out TArgumentData>
     : ICommand
 {
+    /// <summary>The record to which data about the argument is recorded.</summary>
+    public abstract TRecord DataRecord { get; }
+
     /// <summary>The parameter associated with the argument.</summary>
     public abstract TParameter Parameter { get; }
 
